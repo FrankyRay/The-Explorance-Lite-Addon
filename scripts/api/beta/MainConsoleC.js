a41ce35717adb90bebffa559ac83a17dba0e9537
FrankyRay <frankyraymarcellsinaga@gmail.com>
2022-04-19 21:23:22 +0700

    Test Push

--- scripts/api/beta/MainConsoleC.js
+++ scripts/api/beta/MainConsoleC.js
@@ -7,6 +7,8 @@ import {
 import { Print } from "../PrintMessage.js";
 import * as ScoreboardConsC from "./ScoreboardConsoleC.js";
 
+// Test command
+
 const Overworld = world.getDimension("overworld");
 
 export function Ability(player) {
